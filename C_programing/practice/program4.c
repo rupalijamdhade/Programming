bce77dfe4980f94db62fa2e11022db064a8de7a1
rupalijamdhade <rupalibirhade99@gmail.com>
2026-06-07 00:57:49 +0530

    Add files via upload

---
+++ C_programing/practice/program4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,k;
+
+    printf("Enter first number:\n");
+    scanf("%d",&i);
+
+    printf("Enter second number:\n");
+    scanf("%d",&j);
+
+    k=i+j;
+
+    printf("Addidition is:%d\n",k);
+    return 0;
+}
