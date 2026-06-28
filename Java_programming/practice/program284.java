743bdc5750433b1e44d4973c9b19b0ecbed0b285
rupalijamdhade <rupalibirhade99@gmail.com>
2026-06-28 17:11:47 +0530

    Add files via upload

---
+++ Java_programming/practice/program284.java
@@ -0,0 +1,12 @@
+
+class program283
+{
+    public static void main(String A[])
+    {
+        char ch1 = 'm';
+
+        System.out.print(ch1);
+        System.out.print((char)(ch1-32));
+        
+    }
+}
